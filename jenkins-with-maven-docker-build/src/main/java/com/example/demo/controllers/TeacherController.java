e5dd457d3d586824edca6d6069bad7c42cca0f8c
Narayanee.V <Narayanee.V@EXIND3020-87.allfon.in>
2022-03-21 12:50:50 +0530

    REQ-102 get by dept

--- jenkins-with-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
+++ jenkins-with-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,16 @@ public class TeacherController {
 	@GetMapping(path= "/teachers")
 	public String getTeachers() {
 		List<String> list = Arrays.asList("Amar", "akbar", "Anthony");
+		return list.toString();
+	}
+	
+	@GetMapping(path= "/teachers/{dept}")
+	public String getTeachersByDept(@PathVariable("dept") String dept) {
+		List<String> list = Arrays.asList("Amar", "akbar", "Anthony");
+		
+		if(dept.equals("ece")) {
+			list = Arrays.asList("shankar, salim, simon");
+		}
 		return list.toString();
 	}
 }
