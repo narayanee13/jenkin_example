ce3d6673c76a2c4930fa43460f058f7584129951
Narayanee.V <Narayanee.V@EXIND3020-87.allfon.in>
2022-03-21 17:12:35 +0530

    REQ-103 get by id

--- jenkins-with-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
+++ jenkins-with-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -27,4 +27,10 @@ public class TeacherController {
 		}
 		return list.toString();
 	}
+	
+	@GetMapping(path= "/teachers/{id}")
+	public String getTeachersById(@PathVariable("id") String id) {
+		
+		return "hii";
+	}
 }
